d10e01af721484329cb24ff49bacd603b76c7781
nvandy23 <nvandy2373@gmail.com>
2024-05-18 21:54:18 -0400

    intial setup of deck and randomzie deck.

---
+++ How-to-Use-CSS-Card-Library/js/main.js
@@ -0,0 +1,54 @@
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const originalDeck = buildOriginalDeck();
+// renderDeckInContainer(originalDeck, document.getElementById('original-deck-container'));
+let shuffledDeck;
+
+console.log("test")
+
+
+
+const shuffledContainer = document.getElementById('shuffled-deck-container');
+
+
+// document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
+
+function getNewShuffledDeck() {
+  const tempDeck = [...originalDeck];
+  const newShuffledDeck = [];
+  while (tempDeck.length) {
+    const rndIdx = Math.floor(Math.random() * tempDeck.length);
+    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+  }
+  return newShuffledDeck;
+}
+
+function renderNewShuffledDeck() {
+  shuffledDeck = getNewShuffledDeck();
+  // renderDeckInContainer(shuffledDeck, shuffledContainer);
+}
+
+// function renderDeckInContainer(deck, container) {
+//   container.innerHTML = '';
+//   let cardsHtml = '';
+//   deck.forEach(function(card) {
+//     cardsHtml += `<div class="card ${card.face}"></div>`;
+//   });
+//   container.innerHTML = cardsHtml;
+// }
+
+function buildOriginalDeck() {
+  const deck = [];
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        face: `${suit}${rank}`,
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
+
+renderNewShuffledDeck();
