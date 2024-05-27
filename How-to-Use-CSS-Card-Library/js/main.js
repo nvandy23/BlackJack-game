979d95da47a5d658fdfe4cb5c62e8bf2c2e75e0c
nvandy23 <nvandy2373@gmail.com>
2024-05-27 19:39:09 -0400

    add bets container and buttons.

--- How-to-Use-CSS-Card-Library/js/main.js
+++ How-to-Use-CSS-Card-Library/js/main.js
@@ -10,13 +10,11 @@ const playerHandContainer = document.querySelector('#player--hand--container');
 const dealerHandContainer = document.querySelector('#dealer--hand--container');
 const dealerHandValue = document.querySelector('#dealer--hand--value')
 const playerHandValue = document.querySelector('#player--hand--value')
-const playerScoreNum =document.querySelector('#Your--score')
-const dealerScoreNum =document.querySelector('#Dealer--score')
+const playerChips =document.querySelector('#Your--chips')
 const playerHand =[]
 const dealerHand =[]
 
 let playerScore = 0
-let dealerScore = 0
 let playerCard1
 let playerCard2
 let dealerCard1
@@ -86,8 +84,8 @@ playerHandValue.textContent = sum
 for(const score2 of dealerHand) {
   sum2 += score2.value
   dealerHandValue.textContent = sum2
-  playerScoreNum.textContent = playerScore
-  dealerScoreNum.textContent = dealerScore
+  playerChips.textContent = playerScore
+
 }
 
 
